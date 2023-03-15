ee5c889f9ec1bfa260087f578468ebf34a20a23b
tanenrumu <tao0lu@protonmail.com>
2023-03-15 23:29:06 +0800

    gallery

--- lib/pages/search_page/search_page.dart
+++ lib/pages/search_page/search_page.dart
@@ -1,5 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iwrqk/widgets/iwr_appbar.dart';
+import 'package:iwrqk/widgets/iwr_gallery.dart';
+import 'package:iwrqk/widgets/reloadable_image.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -22,6 +28,14 @@ class _SearchPageState extends State<SearchPage> {
             title: Text(
               "Search",
             )),
-        body: Container());
+        body: AspectRatio(
+          aspectRatio: 4 / 3,
+          child: IwrGallery(
+            imageUrls: [
+              "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTNlMDA2NjJjNWRmMDdlOTYwYzUzZTliNTk3NGJmNGRkZDhlZmMwYiZjdD1n/l2JhIUyUs8KDCCf3W/giphy.gif",
+              "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTNlMDA2NjJjNWRmMDdlOTYwYzUzZTliNTk3NGJmNGRkZDhlZmMwYiZjdD1n/l2JhIUyUs8KDCCf3W/giphy.gif"
+            ],
+          ),
+        ));
   }
 }
